a87dc0f0b178a431a9e97d88dd161b4abf6f0ba3
saidu941 <saidu.dosapati@gmail.com>
2019-12-28 20:28:39 -0500

    Added the chatbot-app application

---
+++ Chatbot-app/src/test/java/com/example/Chatbotapp/ChatbotAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Chatbotapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChatbotAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
